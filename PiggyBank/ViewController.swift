8edccfd34148634141e1fdc48168916c5f742019
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-02-15 15:06:25 +0600

    Add the picture of pig

---
+++ PiggyBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PiggyBank
+//
+//  Created by Asol on 2/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello, World!")
+    }
+
+
+}
+
